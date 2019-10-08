da83aa0e7e8de464b20f0d9a2f9b8bc973c6deb2
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-10-08 13:27:57 -0700

    Layout search screen

--- app/src/main/java/com/example/mytube/MainActivity.java
+++ app/src/main/java/com/example/mytube/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.mytube;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchEdit;
+    private Button searchButton;
+    private ListView searchList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchEdit = findViewById(R.id.searchEdit);
+        searchButton = findViewById(R.id.searchButton);
+        searchList = findViewById(R.id.searchList);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = searchEdit.getText().toString();
+                if (!TextUtils.isEmpty(query)) {
+                    String[] array = { query + " 1", query + " 2", query + " 3" };
+                    searchList.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, array));
+                }
+            }
+        });
     }
 }
